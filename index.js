ff34fae969531aee87b3ff4c339e14344bf7a56a
Chris Trevino <chris.trevino@atsid.com>
2015-06-10 16:10:38 -0700

    Using a boring, old es5 function in the index

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ let Parser = require("./dist/parser");
 let Walker = require("./dist/walker");
 let Executor = require("./dist/executor");
 let Runner = require("./dist/http-runner");
-module.exports = (agent) => new Walker(new Parser(), new Executor(agent));
+module.exports = function (agent) {
+    return new Walker(new Parser(), new Executor(agent));
+} ;
