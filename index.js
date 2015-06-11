b33181907126105bbb621b4934982ecb4fd14d70
Chris Trevino <chris.trevino@atsid.com>
2015-06-11 05:23:39 +0000

    Updating index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let Parser = require("./dist/parser");
 let Walker = require("./dist/walker");
 let Executor = require("./dist/executor");
 let Runner = require("./dist/http-runner");
+
 module.exports = function (agent) {
-    return new Walker(new Parser(), new Executor(agent));
+    return new Walker(new Parser(), new Executor(new Runner(agent)));
 } ;
